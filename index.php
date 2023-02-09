cf153f2d97fba135499effcf5233e39e33343ba2
vince <vincent.harpin@orange.fr>
2023-02-09 21:06:01 +0100

    exercice 194

--- index.php
+++ index.php
@@ -13,3 +13,58 @@
  * 4. Faites la même chose, mais cette fois ci, triez le résultat selon la colonne ID, du plus grand au plus petit.
  * 5. Faites la même chose, mais cette fois ci en ne sélectionnant que les noms et les prénoms.
  */
+
+$server = 'localhost';
+$user = 'root';
+$pwd = '';
+$db = 'bdd_cours';
+
+try {
+
+    $maConnexion = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+    $maConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $maConnexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $pom = $maConnexion->prepare("SELECT id, nom, prenom, rue, numero, code postal, ville, pays, mail FROM user");
+
+    $state = $pom->execute();
+
+    if($state) {
+        foreach($pom->fetchAll() as $user) {
+            echo "<div>Personne " . $user['id'] . ":" . $user['nom'] . " " . $user['prenom'] . " " . $user['rue'] . " " . $user['numero'] . " " . $user['code postal'] . " " . $user['ville'] . " " . $user['pays'] . " " . $user['mail'] . "</div>";
+        }
+    }
+    else {
+        echo "Une erreur est survenue en récupérant les données de la table.";
+    }
+
+    $pom = $maConnexion->prepare("SELECT * from user ORDER BY id DESC");
+
+    $state = $pom->execute();
+
+    if($state) {
+        foreach($pom->fetchAll() as $user) {
+            echo "<div>Personne " . $user['id'] . ":" . $user['nom'] . " " . $user['prenom'] . " " . $user['rue'] . " " . $user['numero'] . " " . $user['code postal'] . " " . $user['ville'] . " " . $user['pays'] . " " . $user['mail'] . "</div>";
+        }
+    }
+    else {
+        echo "Une erreur est survenue en récupérant les données de la table.";
+    }
+
+    $pom = $maConnexion->prepare("SELECT nom, prenom FROM user");
+
+    $state = $pom->execute();
+
+    if($state) {
+        foreach($pom->fetchAll() as $user) {
+            echo "<div>Personne :" . $user['nom'] . " " . $user['prenom'] . "</div>";
+        }
+    }
+    else {
+        echo "Une erreur est survenue en récupérant les données de la table.";
+    }
+}
+catch (PDOException $exception) {
+    echo "Erreur de connexion: " . $exception->getMessage();
+}
+
